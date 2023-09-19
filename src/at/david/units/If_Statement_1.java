5ae440fc500aca25286ffc944476e8c63cc24582
David <david.sienz@outlook.com>
2023-09-19 08:56:42 +0200

    init

---
+++ src/at/david/units/If_Statement_1.java
@@ -0,0 +1,25 @@
+package at.david.Units;
+
+import java.util.Random;
+
+public class If_Statement_1 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int randomNumber = random.nextInt(100);
+
+        System.out.println(randomNumber);
+
+
+        if (randomNumber<20){
+            System.out.println("Mini");
+        }
+
+        else if ((randomNumber>20) && (randomNumber<50)){
+            System.out.println("Medium");
+        }
+
+        else {
+            System.out.println("Large");
+        }
+    }
+}
---
+++ src/at/david/units/If_Statement_2.java
@@ -0,0 +1,22 @@
+package at.david.Units;
+
+import java.util.Random;
+
+public class If_Statement_2 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int randomNumber1 = random.nextInt(100);
+        int randomNumber2 = random.nextInt(100);
+
+
+        if (randomNumber1 < 50 && randomNumber1 < randomNumber2) {
+            System.out.println("Zahl 1 ist kleiner als Zahl 2 und Mini");
+
+        } else if (randomNumber1 < 30 || randomNumber2 < randomNumber1) {
+            System.out.println("Eine der beiden ist kleiner als 30");
+
+        } else if (randomNumber1 < 50 && randomNumber2 != 50) {
+            System.out.println("Erste Zahl klein, zweite kein 50iger");
+        }
+    }
+}
