ba194c8c9f2563dd88f81ff4c33915b67be9b769
David <david.sienz@outlook.com>
2023-09-22 08:42:18 +0200

    finished: Loops_Aufgabe_1

---
+++ src/at/david/units/Loops_Aufgabe_1.java
@@ -0,0 +1,16 @@
+
+package at.david.units;
+
+public class Loops_Aufgabe_1 {
+    public static void main(String[] args) {
+        int sum = 0;
+
+            for (int i = 1; i<=100; i++){
+                sum += i;
+            }
+
+
+            System.out.println(sum);
+
+    }
+}
