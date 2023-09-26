bfab549b317ffa70f7a5042c440c8a8aa148cb85
David <david.sienz@outlook.com>
2023-09-26 08:12:36 +0200

    modified switch statement

--- src/at/david/units/Switch_Statement.java
+++ src/at/david/units/Switch_Statement.java
@@ -7,26 +7,13 @@ public class Switch_Statement {
         Random random = new Random();
         int randomNumber1 = random.nextInt(5, 11);
 
-        switch (randomNumber1){
-            case 10:
-                System.out.println("Ten");
-    break;
-            case 9:
-                System.out.println("Nine");
-    break;
-            case 8:
-                System.out.println("Eight");
-    break;
-            case 7:
-                System.out.println("Seven");
-    break;
-            case 6:
-                System.out.println("Six");
-    break;
-            case 5:
-                System.out.println("Five");
-    break;
-
+        switch (randomNumber1) {
+            case 10 -> System.out.println("Ten");
+            case 9 -> System.out.println("Nine");
+            case 8 -> System.out.println("Eight");
+            case 7 -> System.out.println("Seven");
+            case 6 -> System.out.println("Six");
+            case 5 -> System.out.println("Five");
         }
     }
 }
