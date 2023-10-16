9370353315f7b9dc53ccc2ef56468feb0ae8d6a6
David <david.sienz@outlook.com>
2023-10-16 14:44:42 +0200

    excercise to "Arrays"

---
+++ src/at/david/units/Arrays_Test_1.java
@@ -0,0 +1,11 @@
+package at.david.units;
+
+public class Arrays_Test_1 {
+    public static void main(String[] args) {
+        String[] test = {"5", "f", "z"};
+
+        for (int i = 0; i < test.length; i++) {
+            System.out.println(test[i]);
+        }
+    }
+}
