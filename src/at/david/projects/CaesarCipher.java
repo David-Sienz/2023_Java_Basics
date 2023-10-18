fc8e00ae385150269e5a6b71493c8bf64f60f90b
David <david.sienz@outlook.com>
2023-10-18 09:31:57 +0200

    worked on: CaesarCipher

---
+++ src/at/david/projects/CaesarCipher.java
@@ -0,0 +1,29 @@
+package at.david.projects;
+
+public class CaesarCipher {
+    public static void main(String[] args) {
+        String data = "david";
+        char[]
+        char = data.toCharArray();
+
+        String encryptedData = encrypt(data);
+        System.out.println("Das verschlüsselte Wort ist: " + encryptedData);
+
+        String decryptedData = decrypt(encryptedData);
+        System.out.println("Das entschlüsselte Wort ist: " + decryptedData);
+    }
+
+    public static String encrypt(String data) {
+        String encryptedWord = "";
+
+
+        return encryptedWord;
+    }
+
+    public static String decrypt(String data) {
+        String decryptedWord = "";
+
+
+        return decryptedWord;
+    }
+}
