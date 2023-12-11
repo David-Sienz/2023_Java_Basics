fc12a866f01419353c4fd3394cd493bd705ebb01
David <david.sienz@outlook.com>
2023-12-11 14:35:11 +0100

    finished: OO excercise_4.02: Setter & Getter

--- src/at/david/Objektorientierung/Car.java
+++ src/at/david/Objektorientierung/Car.java
@@ -1,48 +1,105 @@
 package at.david.Objektorientierung;
 
 public class Car {
-        //Instanz / Gedächtnisvariablen
+    //Instanz / Gedächtnisvariablen
 
-        //don't do that later
-        public int fuelConsumption;
-        public int fuelAmount;
-        public int tankVolume;
-        public String brand;
-        public String serialNumber;
-        public String color;
-        private double remainingDistance;
+    //don't do that later
+    private int fuelConsumption;
+    private int fuelAmount;
+    private int tankVolume;
+    private String brand;
+    private String serialNumber;
+    private String color;
+    private double remainingDistance;
 
 
-        public Car(int fc, String b, String s){
+       /*public Car(int fc, String b, String s){
                 this.fuelConsumption =fc;
                 this.brand =b;
                 this.serialNumber=s;
-        }
+        }*/
 
-        public void drive(){
-                this.fuelAmount = this.fuelAmount - fuelConsumption;
-                System.out.println("I am driving!");
-        }
+    public void drive() {
+        this.fuelAmount = this.fuelAmount - fuelConsumption;
+        System.out.println("I am driving!");
+    }
 
-        public void brake(){
-                System.out.println("I am breaking!");
-        }
+    public void brake() {
+        System.out.println("I am breaking!");
+    }
 
-        public void turboBoost(){
-                if (fuelAmount > tankVolume * 0.9){
-                        System.out.println("SuperBoostMode is off.");
-                } else{
-                        System.out.println("Not enough fuel to go to Superboost.");
-                }
+    public void turboBoost() {
+        if (fuelAmount > tankVolume * 0.9) {
+            System.out.println("SuperBoostMode is off.");
+        } else {
+            System.out.println("Not enough fuel to go to Superboost.");
         }
-        public void honk(int amountOfRepetations){
-                for (int i = 0; i < amountOfRepetations; i++) {
-                        System.out.println("Tuuut");
-                }
-        }
+    }
 
-        public void getRemainingRange(){
-                this.remainingDistance = this.fuelAmount / this.fuelConsumption;
-                System.out.println("You have " + remainingDistance + " kilometres left!");
+    public void honk(int amountOfRepetations) {
+        for (int i = 0; i < amountOfRepetations; i++) {
+            System.out.println("Tuuut");
         }
+    }
+
+    public void getRemainingRange() {
+        this.remainingDistance = this.fuelAmount / this.fuelConsumption;
+        System.out.println("You have " + remainingDistance + " kilometres left!");
+    }
+
+    public void setTankVolume(int tank) {
+        tankVolume = tank;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setFuelAmount(int fuelAmount) {
+        this.fuelAmount = fuelAmount;
+    }
+
+    public void setFuelConsumption(int fuelConsumption) {
+        this.fuelConsumption = fuelConsumption;
+    }
+
+    public void setRemainingDistance(double remainingDistance) {
+        this.remainingDistance = remainingDistance;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public double getRemainingDistance() {
+        return remainingDistance;
+    }
+
+    public int getFuelAmount() {
+        return fuelAmount;
+    }
+
+    public int getFuelConsumption() {
+        return fuelConsumption;
+    }
+
+    public int getTankVolume() {
+        return tankVolume;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
 }
--- src/at/david/Objektorientierung/Main.java
+++ src/at/david/Objektorientierung/Main.java
@@ -4,31 +4,21 @@ public class Main {
     public static void main(String[] args) {
         int a = 7;
 
-        /*Car c1 = new Car();
-        c1.brand = "Audi";
-        c1.fuelConsumption = 7;
-        c1.serialNumber = "A1234";
-        c1.fuelAmount = 70;
-        c1.color = "red";
-        c1.tankVolume = 100;
+        Car c1 = new Car();
+        c1.setBrand("Audi");
+        c1.setTankVolume(100);
+        c1.setSerialNumber("1234");
+        c1.setFuelAmount(80);
+        c1.setTankVolume(80);
+
+        System.out.println(c1.getBrand());
+        c1.drive();
+        c1.turboBoost();
+        System.out.println(c1.getFuelAmount());
+        System.out.println(c1.getFuelAmount());
         c1.honk(3);
 
-        Car c2 = new Car();
-        c2.brand = "Mercedes";
-        c2.fuelConsumption = 6;
-        c2.serialNumber = "M1234";
-        c2.fuelAmount = 20;
-        c2.color = "black";
-        c2.tankVolume = 120;
-        c2.honk(2);
-
-        System.out.println(c2.fuelAmount);
-        c2.drive();
-        System.out.println(c2.fuelAmount);
-        c2.brake();
-        System.out.println(c2.fuelAmount);*/
-
-        Car c3 = new Car(17, "opel", "01234");
-        System.out.println(c3.serialNumber);
+        /*Car c3 = new Car(17, "opel", "01234");
+        System.out.println(c3.serialNumber);*/
     }
 }
