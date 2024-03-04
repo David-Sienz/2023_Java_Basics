37d3324565ec42bb60884bb477c55b350536b3c7
David <david.sienz@outlook.com>
2024-03-04 14:11:39 +0100

    14 Aufgabe Calculator: finished

--- src/at/david/Objektorientierung/Vererbung/Calculator/Main.java
+++ src/at/david/Objektorientierung/Vererbung/Calculator/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         BaseCalculator b1 = new BaseCalculator();
         TopCalculator t1 = new TopCalculator();
+        ScienceCalculator s1 = new ScienceCalculator();
 
         System.out.println(b1.add(3,5));
         System.out.println(b1.subtract(14,3));
@@ -11,5 +12,8 @@ public class Main {
         System.out.println(b1.divide(4,3));
 
         System.out.println(t1.sqrt(4));
+
+        System.out.println(s1.sinus(180));
+        System.out.println(s1.cosinus(180));
     }
 }
--- src/at/david/Objektorientierung/Vererbung/Calculator/ScienceCalculator.java
+++ src/at/david/Objektorientierung/Vererbung/Calculator/ScienceCalculator.java
@@ -1,11 +1,25 @@
 package at.david.Objektorientierung.Vererbung.Calculator;
 
 public class ScienceCalculator extends TopCalculator {
-    public double sinus(double a){
+    public double sinus(double a) {
+        double sum = 0;
 
+        if (a >= 360 || a <= 360) {
+            sum = Math.sin(a);
+            System.out.print("Der Sinus vom eingegebenen Winkel ist: " + sum);
+        }
+
+        return sum;
     }
 
-    public double cosinus(double a){
+    public double cosinus(double a) {
+        double sum = 0;
 
+        if (a >= 360 || a <= 360) {
+            sum = Math.cos(a);
+            System.out.print("Der Cosinus vom eingegebenen Winkel ist: " + sum);
+        }
+
+        return sum;
     }
 }
